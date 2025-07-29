453e3db181ffc9872d39ca21dd423b269caad8ff
yuvrajsatyapal <yuvrajsatyapal21@gmail.com>
2025-07-30 00:35:49 +0530

    Chat UI

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../library/utils';
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
 
@@ -34,14 +35,25 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
 
               <div className='text-center text-xs'>
                 <img src={msg.senderId === "680f50e4f10f3cd28382ecf9" ? assets.avatar_icon : assets.profile_martin} alt="" className='w-7 rounded-full'/>
-                <p className='text-gray-500'>{msg.createdAt}</p>
+                <p className='text-gray-500'>{formatMessageTime(msg.createdAt)}</p>
               </div>
 
             </div>
           ))} 
 
           <div ref={scrollEnd}>
+          </div>
 
+          <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+            <div className='flex flex-1 items-center bg-gray-100/12 px-3 rounded-full'>
+              <input type="text" placeholder='Send a message' 
+              className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+              <input type="file" id='image' accept='image/png, image/jpeg' hidden />
+              <label htmlFor="image">
+                  <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
+              </label>
+            </div>
+            <img src={assets.send_button} alt="" className='w-7 cursor-pointer'/>
           </div>
 
         </div>
