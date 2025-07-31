e9193c1163279334a0bbf61dabaa3d2f5aedbe56
yuvrajsatyapal <yuvrajsatyapal21@gmail.com>
2025-07-31 14:35:24 +0530

    Context-Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-import ProfilePage from './pages/ProfilePage.jsx'
-
+import React, { useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import { Toaster } from 'react-hot-toast';
+import { AuthContext } from '../context/AuthContext.jsx';
 
 const App = () => {
+  const { authUser } = useContext(AuthContext);
+
   return (
-    <div className="bg-[rgba(44,94,59,1)]">
+    <div className="bg-[rgba(44,94,59,1)] bg-contain min-h-screen">
+      <Toaster />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import assets from '../assets/assets';
+import { AuthContext } from '../../context/AuthContext';
 
 const LoginPage = () => {
 
@@ -10,6 +11,8 @@ const LoginPage = () => {
   const [bio, setBio] = useState("");
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+  const {login} = useContext(AuthContext);
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
     
@@ -17,6 +20,8 @@ const LoginPage = () => {
       setIsDataSubmitted(true);
       return;
     }
+
+    login(currState === "Sign up" ? 'signup' : 'login', {fullName, email, password, bio});
   }
 
 
